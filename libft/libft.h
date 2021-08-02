457bcfe5523299dd12f2b13db494b6ed50ded26c
Joao Alberto <jooalberto1@gmail.com>
2021-08-02 20:58:12 -0300

    adds function prototypes into libft.h

---
+++ libft/libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joalbert <joalbert@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 19:59:21 by joalbert          #+#    #+#             */
+/*   Updated: 2021/08/02 20:22:51 by joalbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+
+# define LIBFT_H
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*strchr(const char *s, int c);
+
+#endif
