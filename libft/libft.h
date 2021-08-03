6c23926da949aba3b969f77abd251b5da6574960
Joao Alberto <jooalberto1@gmail.com>
2021-08-02 21:10:01 -0300

    adds strlcat, strlcpy and strncmp

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: joalbert <joalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 19:59:21 by joalbert          #+#    #+#             */
-/*   Updated: 2021/08/02 20:22:51 by joalbert         ###   ########.fr       */
+/*   Updated: 2021/08/02 21:08:28 by joalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,19 @@
 # define LIBFT_H
 # include <stdlib.h>
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+int				ft_strncmp(char *s1, char *s2, unsigned int n);
 
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*strchr(const char *s, int c);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+
+size_t			ft_strlen(const char *str);
+char			*strchr(const char *s, int c);
 
 #endif
